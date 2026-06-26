4e7f31911ccc5afe63eece134706ce4ce7756263
Dirik_Ali <106819388+RavenRidge0@users.noreply.github.com>
2026-06-26 04:03:28 +0300

    Update check_sequences.sql

--- check_sequences.sql
+++ check_sequences.sql
@@ -1,7 +1,5 @@
--- Показати всі послідовності
 SELECT sequence_name FROM information_schema.sequences;
 
--- Показати поточні максимальні ID
 SELECT 'Cars max Id' as tbl, COALESCE(MAX("Id"), 0) as max_id FROM "Cars"
 UNION ALL
 SELECT 'Brands max Id', COALESCE(MAX("Id"), 0) FROM "Brands";
